109e8760838bf9a84ba796e04e2a35fe23fb7a99
JaunePomme <alexredon75@gmail.com>
2025-01-12 09:35:57 +0100

    son, prettier, ptit css

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
+"use client";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function Home() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleMouseEnter = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0; // Rewind the audio to the start
+      audioRef.current.play();
+    }
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="flex items-center justify-between w-full max-w-6xl p-4 sm:p-6 border-b border-solid border-gray-200">
         <div className="flex items-center gap-4">
-          <Image
-            src="/meteora.svg"
-            alt="meteora logo"
-            width={40}
-            height={40}
-            style={{ borderRadius: "50%" }}
-          />
-          <span className="text-lg font-bold">Japanese Meteora Guide</span>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://meteora.ag/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/meteora.svg"
+              alt="meteora logo"
+              width={40}
+              height={40}
+              style={{ borderRadius: "50%" }}
+            />
+            <span className="text-lg font-bold">Japanese Meteora Guide</span>
+          </a>
         </div>
         <nav className="flex gap-6 text-sm sm:text-base">
           <a
@@ -47,16 +64,42 @@ export default function Home() {
           </a>
         </nav>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
+
+      <div className="floating-button">
+        <a
+          href="#section0"
+          className="hover:underline text-gray-700 dark:text-gray-300"
+        >
           <Image
             src="/jaunepomme.jpg"
             alt="JaunePomme logo"
-            width={180}
-            height={180}
-            style={{ borderRadius: "50%" }}
+            width={80}
+            height={80}
+            style={{ borderRadius: "50%", cursor: "pointer" }}
             priority
+            className={"hoverImage"}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={() => audioRef.current?.pause()} // Optional: Pause the audio when hover ends
           />
+          <audio ref={audioRef} src="/psy1.mp3" />
+        </a>
+      </div>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <section id="section0" className="w-full max-w-6xl mx-auto px-4 py-8">
+          <>
+            <Image
+              src="/jaunepomme.jpg"
+              alt="JaunePomme logo"
+              width={180}
+              height={180}
+              style={{ borderRadius: "50%" }}
+              priority
+              className={"hoverImage"}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={() => audioRef.current?.pause()} // Optional: Pause the audio when hover ends
+            />
+            <audio ref={audioRef} src="/psy1.mp3" />
+          </>
           Since I never saw a Japanese guide for Meteora I decided to start one
           to support the Japanese community. So here is my contribution.
           <p>
@@ -70,7 +113,7 @@ export default function Home() {
             💁マジで美味いよねC.C.Lemonって では、相棒、準備は良いか?
             行こう、遥か彼方へ。
           </p>
-        </div>
+        </section>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -219,7 +262,7 @@ export default function Home() {
               <p className="text-lg">
                 ではまず Meteora App,こちらに行きましょう:
               </p>
-              <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <div className="flex gap-4 items-center flex-col sm:flex-row justify-center">
                 <a
                   className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                   href="https://app.meteora.ag/"
